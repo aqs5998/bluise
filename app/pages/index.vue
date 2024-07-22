067623c8c9a1cc8692742988a737a62375cc975c
aqs5998 <57415479+aqs5998@users.noreply.github.com>
2024-07-21 22:05:36 -0600

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,7 @@
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
         <div class="mb-12 xl:mb-0">
-         <h2 class="phone-number">{{ phoneNumber }}</h2>
+         <h1 class="phone-number">{{ phoneNumber }}</h1>
         </div>
         </div>
       <div class="flex flex-col w-full xl:w-2/5">
