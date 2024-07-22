ce2bc81eccdbb5209fb6d84e790fc1ea0c4adb08
aqs5998 <57415479+aqs5998@users.noreply.github.com>
2024-07-21 21:57:48 -0600

    Update footer.vue

--- app/components/partials/footer.vue
+++ app/components/partials/footer.vue
@@ -3,7 +3,7 @@
     <div class="container mx-auto pb-6 flex flex-col items-center">
       <div class="header__logo">
         <nuxt-link to="/">
-          <img src="/images/uploads/logo.svg" alt="Logo" />
+          <img src="/images/uploads/logo.png" alt="Logo" />
         </nuxt-link>
       </div>
       <div class="footer__text mt-4 text-center">
